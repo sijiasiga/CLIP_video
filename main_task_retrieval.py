2fd45049f455ddef7ededa1ab214f337df4f3fbf
sijiasiga <105508469+sijiasiga@users.noreply.github.com>
2025-04-06 21:47:29 -0400

    Update main_task_retrieval.py

--- main_task_retrieval.py
+++ main_task_retrieval.py
@@ -14,7 +14,7 @@ from modules.tokenization_clip import SimpleTokenizer as ClipTokenizer
 from modules.file_utils import PYTORCH_PRETRAINED_BERT_CACHE
 from modules.modeling import CLIP4Clip
 from modules.optimization import BertAdam
-from tqdm.notebook import tqdm
+from tqdm import tqdm
 
 
 from util import parallel_apply, get_logger
